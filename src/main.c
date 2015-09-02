77ca3c33c735042f0e02549d26d352265fc0729c
starscreamsghost <donryan222@gmail.com>
2015-09-02 10:19:33 -0500

    initial check in of vacation

--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
 #include <pebble.h>
-  //creates the static pointer for the window
-  static Window *s_main_window;
- //create the static pointer for the time layer 
-  static TextLayer *s_time_layer;
   
-
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+  
+//creates the static pointer for the window
+  static Window *s_main_window;
+//create the static pointer for the time layer 
+  static TextLayer *s_time_layer;
+  static TextLayer *s_weather_layer;
+  static BitmapLayer *s_background_layer;
+  static GBitmap *s_background_bitmap;
+//weather font
+  static GFont s_weather_font;
+  
 static void update_time(){
   //Get tm structure
   time_t temp = time(NULL);
@@ -25,31 +33,111 @@ static void update_time(){
   //Display the time on the textlayer
   text_layer_set_text(s_time_layer, buffer);
 }
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
-  update_time();
-}
 
 //instance of the loading of the window
 static void main_window_load(Window *window){
+  //Create the GBitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PALM_TREE);
+  s_background_layer = bitmap_layer_create(GRect(0,0,144,168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  
   //Create the text layer
   s_time_layer = text_layer_create(GRect(0,55,144,50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  //text_layer_set_text(s_time_layer, "00:00"); Remove later just a place holder
   
   //Improve the layer to more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
   //Add it as a child layer to Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  // Create temperature Layer
+  s_weather_layer = text_layer_create(GRect(0, 100, 144, 25));
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorBlack);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
+  //create weather font
+  text_layer_set_font(s_weather_layer,fonts_get_system_font(FONT_KEY_GOTHIC_14) );
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  // Make sure the time is displayed from the start
+  update_time();
 }
 //instance of the unloading of the window
 static void main_window_unload(Window *window){
   //Destroy the text layer
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
   text_layer_destroy(s_time_layer);
+  // Destroy weather elements
+  text_layer_destroy(s_weather_layer);
+  fonts_unload_custom_font(s_weather_font);
 }
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+  
+  // Get weather update every 30 minutes
+  if(tick_time->tm_min % 30 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+
+    // Send the message!
+    app_message_outbox_send();
+  }
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+  
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    case KEY_TEMPERATURE:
+      snprintf(temperature_buffer, sizeof(temperature_buffer), "%dF", (int)t->value->int32);
+      break;
+    case KEY_CONDITIONS:
+      snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+      break;
+    default:
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
+    }
+
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+  
+  // Assemble full string and display
+  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+  text_layer_set_text(s_weather_layer, weather_layer_buffer);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+  
   static void init(){
   //Create the main Window and assigned to pointer
     s_main_window = window_create();
@@ -65,8 +153,17 @@ static void main_window_unload(Window *window){
     //Display time from the start 
     update_time();
     //Registers with TickTimer
-    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-}
+   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  }
 
 static void deinit(){
   //Destroy Window
